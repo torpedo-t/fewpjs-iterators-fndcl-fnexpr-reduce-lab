8396e7acd0b26b5a4322da5779629b6f41b17be9
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-02 13:49:52 -0500

    commit tree f2de49f04d8d40318de91d154d2c119d1f0332c9

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ return element + accumulator }, 0)
+
+
 // Code your solution here
+// const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 0)
+// incomingStudents.reduce(function(houses, student) { houses[sortingHat.assign(student)].push(student)}, hogwartsHouses)
